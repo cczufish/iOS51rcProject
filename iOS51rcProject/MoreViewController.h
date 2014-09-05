ad6397c51c11d1413b3aada3e61105fdd106efa5
https://github.com/madyson/iOS51rcProject.git <lucifer@LuciferdeMacBook-Air.local>
2014-09-05 10:38:25 +0800

    1
    1

---
+++ iOS51rcProject/MoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreViewController.h
+//  iOS51rcProject
+//
+//  Created by Lucifer on 14-9-5.
+//  Copyright (c) 2014年 Lucifer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreViewController : UIViewController
+
+@end
