577c3b99e770f4382df4a731dda3b44dbfe30740
Andy-Yin <Lucifer@oukaixintekiMac-mini.local>
2014-09-25 16:19:57 +0800

    企业邀约，无面试通知，应显示无面试通知提示页面

--- iOS51rcProject/UserCenter/InterviewNoticeViewController.m
+++ iOS51rcProject/UserCenter/InterviewNoticeViewController.m
@@ -39,6 +39,8 @@
     //数据加载等待控件初始化
     loadView = [[LoadingAnimationView alloc] initWithFrame:CGRectMake(140, 100, 80, 98) loadingAnimationViewStyle:LoadingAnimationViewStyleCarton target:self];
     //[self onSearch];
+    //不显示列表分隔线
+    self.tvReceivedInvitationList.separatorStyle = UITableViewCellSeparatorStyleNone;
    
 }
 
@@ -55,11 +57,45 @@
               responseData:(NSMutableArray *)requestData
 {
     if (request.tag == 1) {
-        [self.recruitmentCpData removeAllObjects];
-        self.recruitmentCpData = requestData;
+        if (requestData.count>0) {
+            [self.recruitmentCpData removeAllObjects];
+            self.recruitmentCpData = requestData;
+            
+            [self.tvReceivedInvitationList reloadData];
+            [self.tvReceivedInvitationList footerEndRefreshing];
+        }else{
+            //没有面试通知记录醒
+            self.lbMessage.text = @" ";
+            self.lbMessage.layer.borderColor = [UIColor whiteColor].CGColor;
+            
+            UIView *viewHsaNoCv = [[[UIView alloc] initWithFrame:CGRectMake(20, 100, 240, 80)]autorelease];
+            UIImageView *img = [[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 40, 60)] autorelease];
+            img.image = [UIImage imageNamed:@"pic_noinfo.png"];
+            [viewHsaNoCv addSubview:img];
+            
+            UILabel *lb1 = [[[UILabel alloc]initWithFrame:CGRectMake(50, 10, 220, 20)] autorelease];
+            lb1.text = @"亲，您没有面试通知记录,申请的";
+            lb1.font = [UIFont systemFontOfSize:14];
+            lb1.textAlignment = NSTextAlignmentCenter;
+            [viewHsaNoCv addSubview:lb1];
+            
+            UILabel *lb2 = [[[UILabel alloc] initWithFrame:CGRectMake(50, 30, 300, 20)] autorelease];
+            lb2.text = @"职位越多，收到的面试通知就会越多。";
+            lb2.font = [UIFont systemFontOfSize:14];
+            lb2.textAlignment = NSTextAlignmentLeft;
+            [viewHsaNoCv addSubview:lb2];
+            
+            UILabel *lb3 = [[[UILabel alloc] initWithFrame:CGRectMake(50, 50, 200, 20)] autorelease];
+            lb3.text = @"现在就去申请职位吧。";
+            lb3.font = [UIFont systemFontOfSize:14];
+            lb3.textColor =  [UIColor colorWithRed:255.f/255.f green:90.f/255.f blue:39.f/255.f alpha:1];
+            lb3.textAlignment = NSTextAlignmentCenter;
+            [viewHsaNoCv addSubview:lb3];
+            
+            [self.view addSubview:viewHsaNoCv];
+
+        }
         
-        [self.tvReceivedInvitationList reloadData];
-        [self.tvReceivedInvitationList footerEndRefreshing];
         //结束等待动画
         [loadView stopAnimating];
     }
@@ -69,7 +105,7 @@
     }
     else if(request.tag == 3)
     {
-        if (requestData != nil) {
+        if (requestData.count > 0) {
             //如果有简历，才查询数据
             [self.recruitmentCpData removeAllObjects];
             [self.tvReceivedInvitationList reloadData];
@@ -89,26 +125,30 @@
         }
         else{
             //没有简历的提醒
-            UIView *viewHsaNoCv = [[[UIView alloc] initWithFrame:CGRectMake(40, 100, 240, 80)]autorelease];
+            self.lbMessage.text = @" ";
+            self.lbMessage.layer.borderColor = [UIColor whiteColor].CGColor;
+            
+            UIView *viewHsaNoCv = [[[UIView alloc] initWithFrame:CGRectMake(30, 100, 240, 80)]autorelease];
             UIImageView *img = [[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 40, 60)] autorelease];
             img.image = [UIImage imageNamed:@"pic_noinfo.png"];
             [viewHsaNoCv addSubview:img];
             
-            UILabel *lb1 = [[[UILabel alloc]initWithFrame:CGRectMake(50, 10, 180, 20)] autorelease];
+            UILabel *lb1 = [[[UILabel alloc]initWithFrame:CGRectMake(50, 10, 220, 20)] autorelease];
             lb1.text = @"亲，您没有完整的简历";
-            lb1.font = [UIFont systemFontOfSize:15];
+            lb1.font = [UIFont systemFontOfSize:14];
             lb1.textAlignment = NSTextAlignmentCenter;
             [viewHsaNoCv addSubview:lb1];
             
-            UILabel *lb2 = [[[UILabel alloc] initWithFrame:CGRectMake(50, 30, 160, 20)] autorelease];
+            UILabel *lb2 = [[[UILabel alloc] initWithFrame:CGRectMake(50, 30, 150, 20)] autorelease];
             lb2.text = @"HR关注不到您，建议您";
-            lb2.font = [UIFont systemFontOfSize:15];
+            lb2.font = [UIFont systemFontOfSize:14];
             lb2.textAlignment = NSTextAlignmentLeft;
             [viewHsaNoCv addSubview:lb2];
             
-            UILabel *lb3 = [[[UILabel alloc] initWithFrame:CGRectMake(lb2.frame.origin.x, 30, 160, 20)] autorelease];
+            UILabel *lb3 = [[[UILabel alloc] initWithFrame:CGRectMake(lb2.frame.origin.x + lb2.frame.size.width - 5, 30, 140, 20)] autorelease];
             lb3.text = @"立即完善简历";
-            lb3.font = [UIFont systemFontOfSize:15];
+            lb3.font = [UIFont systemFontOfSize:14];
+            lb3.textColor =  [UIColor colorWithRed:255.f/255.f green:90.f/255.f blue:39.f/255.f alpha:1];
             lb3.textAlignment = NSTextAlignmentLeft;
             [viewHsaNoCv addSubview:lb3];
             
@@ -384,6 +424,11 @@
     }else{
         selectRowHeight = 100;
     }
+    //分割线
+    UIView *viewSeparate = [[UIView alloc] initWithFrame:CGRectMake(0, selectRowHeight - 2, 320, 0.5)];
+    [viewSeparate setBackgroundColor:[UIColor lightGrayColor]];
+    [cell.contentView addSubview:viewSeparate];
+    
     return cell;
 }
 
