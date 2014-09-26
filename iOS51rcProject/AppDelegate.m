08e9eb1fda2f14725944f08633305fcbb266adce
https://github.com/madyson/iOS51rcProject.git <lucifer@LuciferdeMacBook-Air.local>
2014-09-26 08:37:37 +0800

    禁止横屏修改

--- iOS51rcProject/AppDelegate.m
+++ iOS51rcProject/AppDelegate.m
@@ -259,4 +259,9 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (NSUInteger)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window
+{
+    return UIInterfaceOrientationMaskPortrait;
+}
+
 @end
