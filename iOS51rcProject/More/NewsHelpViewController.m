37fd0d24ba0f596706c53b76030aed728e151ae1
Andy-Yin <Lucifer@oukaixintekiMac-mini.local>
2014-10-08 12:03:59 +0800

    43、欢迎四个页面 没加切换的点。四个欢迎图片，没有更新成May最后发的，最新的。现在用的图片的字体，有版权问题。

--- iOS51rcProject/More/NewsHelpViewController.m
+++ iOS51rcProject/More/NewsHelpViewController.m
@@ -14,7 +14,7 @@
 	[self.navigationController setNavigationBarHidden:YES animated:YES];
     self.view.backgroundColor = [UIColor whiteColor];
     
-    //self.pageScroll.delegate = self;
+    self.pageScroll.delegate = self;
     self.pageScroll.directionalLockEnabled = YES;
     [self.pageScroll setShowsHorizontalScrollIndicator:NO];
      [self.pageScroll setShowsVerticalScrollIndicator:NO];
@@ -56,6 +56,24 @@
     
     self.pageScroll.frame =CGRectMake(0, 0, 320, HEIGHT);
     self.pageScroll.contentSize = CGSizeMake(4*320, HEIGHT);
+    
+    //切换页面的小点
+    self.pager = [[UIPageControl alloc] init];
+    self.pager.frame = CGRectMake(110,404,100,50);
+    [self.pager setNumberOfPages:4];
+    self.pager.currentPage = 0;
+    self.pager.currentPageIndicatorTintColor = [UIColor blackColor];
+    self.pager.pageIndicatorTintColor   = [UIColor grayColor];
+    [self.pager addTarget:self action:@selector(pageTurn:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:self.pager];
+}
+
+-(void)pageTurn:(UIPageControl*)aPageControl
+{
+    CGRect frame = self.pageScroll.frame;
+    frame.origin.x = frame.size.width * aPageControl.currentPage;
+    frame.origin.y = 0;
+    [self.pageScroll scrollRectToVisible:frame animated:YES];
 }
 
 //返回上一页面
@@ -71,6 +89,7 @@
 {
     CGPoint offset = scrollView.contentOffset;
     self.pager.currentPage = offset.x/320 ;
+    //pageControl.currentPage = offset.x/320 ;
 }
 
 
@@ -78,6 +97,7 @@
 {
     CGPoint offset = scrollView.contentOffset;
     self.pager.currentPage = offset.x / 320;
+    //pageControl.currentPage = offset.x/320 ;
 }
 
 
